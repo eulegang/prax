eaffa594cc9b3f0cd9a38e0edcc0e94ce453c4e8
eulegang <eulegang@eulegang.dev>
2023-11-18 10:15:09 -0600

    Adding basic lua config setup

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -1,11 +1,15 @@
 use clap::Parser;
-use std::net::SocketAddr;
+use std::{net::SocketAddr, path::PathBuf};
 
 #[derive(Parser)]
 pub struct Cli {
     /// Attack endpoint
     #[clap(short, long, default_value = "127.0.0.1:8091")]
     pub listen: SocketAddr,
+
+    /// Configure script
+    #[clap(short = 'f', long = "file")]
+    pub configure: Option<PathBuf>,
 
     /// trace all requests
     #[clap(short, long)]
--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,16 @@ async fn main() -> eyre::Result<()> {
 
     let cli = cli::Cli::parse();
 
+    if let Some(path) = cli.configure {
+        let lua = mlua::Lua::new();
+
+        let content = tokio::fs::read_to_string(path).await?;
+
+        let chunk = lua.load(content).set_name("atkpx-config");
+
+        chunk.exec_async().await?;
+    }
+
     let mode = if cli.trace { Mode::Trace } else { Mode::Pass };
 
     listen::listen(cli.listen, Proxy { mode }.into()).await?;
