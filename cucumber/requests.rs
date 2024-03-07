5860144bef9857c137322455a3e6e81c78bcadee
eulegang <eulegang@eulegang.dev>
2024-03-06 23:10:48 -0600

    Making cucumber test more req oriented (res to follow)

--- cucumber/requests.rs
+++ cucumber/requests.rs
@@ -1,8 +1,7 @@
 use std::str::FromStr;
 
 use cucumber::{cli, given, then, when, Parameter, World};
-use http::{uri::PathAndQuery, HeaderName, HeaderValue, Method, StatusCode};
-use hyper::{Request, Response};
+use http::{uri::PathAndQuery, HeaderName, HeaderValue, Method};
 
 use prax::{
     proxy::{Config, Query},
@@ -10,17 +9,8 @@ use prax::{
 };
 
 #[derive(Debug, Default, World)]
-pub struct HttpWorld {
-    subject: Subject,
-}
-
-#[derive(Default, Debug)]
-enum Subject {
-    #[default]
-    Init,
-
-    Request(prax::Req<Vec<u8>>),
-    Response(prax::Res<Vec<u8>>),
+pub struct ReqWorld {
+    subject: prax::Req<Vec<u8>>,
 }
 
 #[derive(Debug, Default, Eq, Parameter, PartialEq)]
@@ -36,204 +26,115 @@ impl std::str::FromStr for Meth {
 }
 
 #[given(expr = "a {meth} request")]
-fn given_method(world: &mut HttpWorld, method: Meth) {
-    let mut req = Request::new(vec![]);
-    *req.method_mut() = method.0;
-    world.subject = Subject::Request(req);
-}
-
-#[given(expr = "a {} response")]
-fn given_status(world: &mut HttpWorld, status: u16) {
-    let mut res = Response::new(vec![]);
-    *res.status_mut() = StatusCode::from_u16(status).unwrap();
-
-    world.subject = Subject::Response(res);
+fn given_method(world: &mut ReqWorld, method: Meth) {
+    *world.subject.method_mut() = method.0;
 }
 
 #[given(expr = "a header {} is {}")]
-fn given_header(world: &mut HttpWorld, name: String, value: String) {
+fn given_header(world: &mut ReqWorld, name: String, value: String) {
     let name = HeaderName::try_from(name).unwrap();
     let value = HeaderValue::try_from(value).unwrap();
 
-    match &mut world.subject {
-        Subject::Init => todo!(),
-        Subject::Request(request) => {
-            request.headers_mut().insert(name, value);
-        }
-        Subject::Response(response) => {
-            response.headers_mut().insert(name, value);
-        }
-    }
+    world.subject.headers_mut().insert(name, value);
 }
 
 #[given(expr = "a query {} is {}")]
-fn given_query(world: &mut HttpWorld, name: String, value: String) {
-    match &mut world.subject {
-        Subject::Init => panic!("uninited"),
-        Subject::Request(request) => {
-            let uri = request.uri_mut().clone();
-            let mut parts = uri.into_parts();
-            if let Some(pq) = &mut parts.path_and_query {
-                let mut q = pq.query().map(Query::from).unwrap_or_default();
-                q.push(&name, Some(&value));
+fn given_query(world: &mut ReqWorld, name: String, value: String) {
+    let uri = world.subject.uri().clone();
+    let mut parts = uri.into_parts();
+    if let Some(pq) = &mut parts.path_and_query {
+        let mut q = pq.query().map(Query::from).unwrap_or_default();
+        q.push(&name, Some(&value));
 
-                *pq = q.to_path_and_query(pq.path()).unwrap();
-            } else {
-                let mut q = Query::default();
-                q.push(&name, Some(&value));
-                parts.path_and_query = Some(q.to_path_and_query("").unwrap())
-            }
-
-            *request.uri_mut() = hyper::Uri::from_parts(parts).unwrap();
-        }
-        Subject::Response(_) => {
-            panic!("can't modify the query of a response")
-        }
+        *pq = q.to_path_and_query(pq.path()).unwrap();
+    } else {
+        let mut q = Query::default();
+        q.push(&name, Some(&value));
+        parts.path_and_query = Some(q.to_path_and_query("").unwrap())
     }
+
+    *world.subject.uri_mut() = hyper::Uri::from_parts(parts).unwrap();
 }
 
 #[given(expr = "a path {}")]
-fn given_path(world: &mut HttpWorld, path: String) {
-    match &mut world.subject {
-        Subject::Init => panic!("uninited"),
-        Subject::Request(request) => {
-            let uri = request.uri_mut().clone();
-            let mut parts = uri.into_parts();
-            if let Some(pq) = &mut parts.path_and_query {
-                if let Some(q) = pq.query() {
-                    *pq = PathAndQuery::from_str(&format!("{}?{}", path, q)).unwrap();
-                } else {
-                    *pq = PathAndQuery::from_str(&path).unwrap();
-                }
-            } else {
-                parts.path_and_query = Some(PathAndQuery::from_str(&path).unwrap());
-            }
-
-            *request.uri_mut() = hyper::Uri::from_parts(parts).unwrap();
+fn given_path(world: &mut ReqWorld, path: String) {
+    let uri = world.subject.uri().clone();
+    let mut parts = uri.into_parts();
+    if let Some(pq) = &mut parts.path_and_query {
+        if let Some(q) = pq.query() {
+            *pq = PathAndQuery::from_str(&format!("{}?{}", path, q)).unwrap();
+        } else {
+            *pq = PathAndQuery::from_str(&path).unwrap();
         }
-        Subject::Response(_) => {
-            panic!("can't modify the path of a response")
-        }
+    } else {
+        parts.path_and_query = Some(PathAndQuery::from_str(&path).unwrap());
     }
+
+    *world.subject.uri_mut() = hyper::Uri::from_parts(parts).unwrap();
 }
 
 #[given(expr = "a body {}")]
-fn given_body(world: &mut HttpWorld, body: String) {
-    match &mut world.subject {
-        Subject::Init => panic!("uninited"),
-        Subject::Request(request) => *request.body_mut() = body.as_bytes().to_vec(),
-        Subject::Response(response) => *response.body_mut() = body.as_bytes().to_vec(),
-    }
+fn given_body(world: &mut ReqWorld, body: String) {
+    *world.subject.body_mut() = body.as_bytes().to_vec();
 }
 
 #[when(expr = "filtered {}")]
-async fn filter(world: &mut HttpWorld, config: String) {
+async fn filter(world: &mut ReqWorld, config: String) {
     let mut pre = r#"target("example.com:3000"):"#.to_string();
     pre.push_str(&config);
     let config: &'static str = String::leak(pre); // we don't care about leaks in tests
     let config = Config::test(&config, ()).await.unwrap();
 
-    match &mut world.subject {
-        Subject::Init => panic!("uninited"),
-        Subject::Request(req) => {
-            let _ = config
-                .modify_request("example.com:3000", req)
-                .await
-                .unwrap();
-        }
-        Subject::Response(res) => {
-            let _ = config
-                .modify_response("example.com:3000", res)
-                .await
-                .unwrap();
-        }
-    }
+    let _ = config
+        .modify_request("example.com:3000", &mut world.subject)
+        .await
+        .unwrap();
 }
 
 #[then(expr = "method is {meth}")]
-fn then_method(world: &mut HttpWorld, method: Meth) {
-    match &world.subject {
-        Subject::Init => panic!("uninited"),
-        Subject::Request(req) => {
-            assert_eq!(req.method(), method.0)
-        }
-        Subject::Response(_) => todo!(),
-    }
-}
-
-#[then(expr = "status is {}")]
-fn then_status(world: &mut HttpWorld, status: u16) {
-    match &mut world.subject {
-        Subject::Init => panic!("uninited"),
-        Subject::Request(_) => panic!("can't assert status of request"),
-        Subject::Response(response) => {
-            assert_eq!(response.status().as_u16(), status);
-        }
-    }
+fn then_method(world: &mut ReqWorld, method: Meth) {
+    assert_eq!(world.subject.method(), method.0)
 }
 
 #[then(expr = "query {} is {}")]
-fn then_query(world: &mut HttpWorld, name: String, value: String) {
-    match &mut world.subject {
-        Subject::Init => panic!("uninited"),
-        Subject::Response(_) => panic!("can not assert the query of a response"),
-        Subject::Request(request) => {
-            let query = request
-                .uri()
-                .path_and_query()
-                .and_then(|pq| pq.query())
-                .map(Query::from)
-                .unwrap_or_default();
+fn then_query(world: &mut ReqWorld, name: String, value: String) {
+    let query = world
+        .subject
+        .uri()
+        .path_and_query()
+        .and_then(|pq| pq.query())
+        .map(Query::from)
+        .unwrap_or_default();
 
-            for (k, v) in query.iter() {
-                let Some(v) = v else {
-                    continue;
-                };
+    for (k, v) in query.iter() {
+        let Some(v) = v else {
+            continue;
+        };
 
-                if k == name && v == value {
-                    return;
-                }
-            }
-
-            panic!("could not find {name}={value} in {query}")
+        if k == name && v == value {
+            return;
         }
     }
+
+    panic!("could not find {name}={value} in {query}")
 }
 
 #[then(expr = "path is {}")]
-fn then_path(world: &mut HttpWorld, path: String) {
-    match &world.subject {
-        Subject::Init => panic!("uninited"),
-        Subject::Response(_) => panic!("can not assert the path of a response"),
-        Subject::Request(request) => {
-            assert_eq!(request.uri().path(), path)
-        }
-    }
+fn then_path(world: &mut ReqWorld, path: String) {
+    assert_eq!(world.subject.uri().path(), path)
 }
 
 #[then(expr = "body is {}")]
-fn then_body(world: &mut HttpWorld, body: String) {
-    match &world.subject {
-        Subject::Init => panic!("uninited"),
-        Subject::Request(request) => {
-            assert_eq!(request.body(), body.as_bytes())
-        }
-        Subject::Response(response) => {
-            assert_eq!(response.body(), body.as_bytes())
-        }
-    }
+fn then_body(world: &mut ReqWorld, body: String) {
+    assert_eq!(world.subject.body(), body.as_bytes())
 }
 
 #[then(expr = "header {} is {}")]
-fn then_header(world: &mut HttpWorld, name: String, value: String) {
+fn then_header(world: &mut ReqWorld, name: String, value: String) {
     let name = HeaderName::try_from(name).unwrap();
     let value = HeaderValue::try_from(value).unwrap();
 
-    let map = match &world.subject {
-        Subject::Init => panic!("uninited"),
-        Subject::Request(req) => req.headers(),
-        Subject::Response(res) => res.headers(),
-    };
+    let map = world.subject.headers();
 
     for (n, v) in map {
         if n == name && v == value {
@@ -260,7 +161,7 @@ fn main() {
         .unwrap();
 
     rt.block_on(async {
-        HttpWorld::cucumber()
+        ReqWorld::cucumber()
             .with_cli(opts)
             .run_and_exit("cucumber/requests")
             .await;
